658e21a468b69ff6de3ae3772078e5b26a2fb988
oalmalky5 <oalmalky5@gmail.com>
2022-04-26 20:51:00 +0300

    boolean to string

--- CC-26-04-2022/app.js
+++ CC-26-04-2022/app.js
@@ -19,3 +19,11 @@ let uniqueInOrder = function(iterable){
     return uniqueArr
   
   }
+
+//   Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    return b === true? "true" : "false"
+  }
