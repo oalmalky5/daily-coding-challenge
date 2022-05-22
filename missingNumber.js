1b6ef2f39e281536a5fd1da92fa65325b2fc071f
oalmalky5 <oalmalky5@gmail.com>
2022-05-22 22:50:30 +0300

    challenge: find the missing number from 0-100

---
+++ missingNumber.js
@@ -0,0 +1,21 @@
+// The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+
+// You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+
+function missingNo(nums) {
+    let incr = 0
+    let missingNum = 0
+    for (let i = 1; i <= nums.length; i++){
+      incr += 1
+      if (nums.includes(incr) === false){
+        missingNum = incr
+      }
+    }
+    return missingNum
+  }
+  
+  console.log(missingNo( [26, 0, 75, 87, 33, 52, 37, 59, 27, 4, 54, 15, 24, 7, 21, 82, 98, 79, 34, 25, 1, 99, 5, 10, 96, 97, 65, 85, 47, 28, 81, 70, 74, 11, 38, 45, 84, 13, 41, 2, 86, 39, 29, 43, 19, 31, 18, 58, 14, 77, 69, 32, 6, 66, 61, 62, 50, 53, 8, 80, 72, 9, 68, 35, 42, 73, 83, 71, 92, 95, 63, 51, 16, 17, 64, 22, 44, 91, 30, 76, 12, 3, 46, 60, 36, 56, 88, 89, 100, 78, 90, 49, 55, 48, 23, 93, 57, 67, 20, 94]
+  ))
+
+
+  // the answer for the above will be 40
