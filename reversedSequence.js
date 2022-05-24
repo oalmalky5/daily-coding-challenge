32cb19c35e06e27670e8da2be4d93a4e39a00669
oalmalky5 <oalmalky5@gmail.com>
2022-05-24 23:40:12 +0300

    challenge: reverse the sequence

---
+++ reversedSequence.js
@@ -0,0 +1,24 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    let finalArr = []
+    for (let i = 0; i < n; i++){
+      finalArr.push(n - i)
+    }
+    
+    return finalArr
+  };
+  
+
+//   a better solution 
+
+
+const ReverseSeq = n => {
+    let arr = [];
+      for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+    };
+    
