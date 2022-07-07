57b17d3bc7b48d1bde6c0176bb4a14fc7c2eb7be
oalmalky5 <oalmalky5@gmail.com>
2022-07-07 23:32:42 +0300

    challenge: return the integer in human readable time

---
+++ humanRaeadableTime.js
@@ -0,0 +1,22 @@
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+
+function humanReadable(seconds) {
+    let hours = parseInt( seconds / 3600 ) ;
+    let minutes = parseInt( seconds / 60 ) % 60;
+    var seconds = seconds % 60;
+  
+    const pad = function(val){
+      return val < 10 ?"0"+val:val;
+    }
+  
+    return pad(hours) + ":" +pad(minutes) + ":" + pad(seconds);
+  }
+  
