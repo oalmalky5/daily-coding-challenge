98e95c75807f63f09af4afa3e85b513ff5a430d8
oalmalky5 <oalmalky5@gmail.com>
2022-08-29 08:30:06 +0300

    challenge: mumbling

---
+++ mumbling.js
@@ -0,0 +1,9 @@
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+
+function accum(s) {
+    return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+  }
+  
